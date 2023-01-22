318a3c96d1f7857ceabaae90d59f67de931c9a50
Tiago Oliveira <contato@otao.dev.br>
2023-01-22 21:43:28 +0000

    feat: ensure CreateCustomerController returns 422 if document already exists

--- app/Http/Controllers/CreateCustomerController.php
+++ app/Http/Controllers/CreateCustomerController.php
@@ -3,17 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Customer;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CreateCustomerController extends Controller
 {
-    public function handle(Request $request): void
+    /**
+     * Summary of handle
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function handle(Request $request): JsonResponse
     {
         $this->validate($request, [
             "first_name" => "required",
             "last_name" => "required",
-            "document" => "required|numeric|digits:11",
+            "document" => "required|numeric|digits:11|unique:customers",
             "birth_date" => "required|date"
         ]);
+
+        $params = $request->all();
+
+        $response = new Customer($params);
+
+        $response->save();
+
+        return response()->json($response, 201);
     }
 }
