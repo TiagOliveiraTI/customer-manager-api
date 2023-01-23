1175990c1af45701e43aa9447ef6588d00ec2d22
Tiago Oliveira <contato@otao.dev.br>
2023-01-23 19:19:58 +0000

    test: ensure ListAllCustomersController saves data in database

--- tests/Feature/App/Http/Controllers/ListAllCustomersControllerTest.php
+++ tests/Feature/App/Http/Controllers/ListAllCustomersControllerTest.php
@@ -19,4 +19,22 @@ class ListAllCustomersControllerTest extends TestCase
             $this->response->getContent()
         );
     }
+
+    public function testListAllCustomers()
+    {
+        $data1 = [
+            'first_name' => 'any_first_name',
+            'last_name' => 'any_last_name',
+            'document' => '01234567890',
+            'birth_date' => '1988-01-31',
+            'phone_number' => '11998765432'
+        ];
+
+        $this->post('/customers', $data1);
+
+        $this->get('/customers');
+
+        $this->seeInDatabase('customers', $data1);
+
+    }
 }
